3a992e854c12c32467d52b99a632fdc3e4831a4b
Codeur Libre <librecodeur@gmail.com>
2023-02-19 12:47:30 +0000

    Projet scroll navbar terminé

--- index.js
+++ index.js
@@ -5,3 +5,28 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+
+var lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  console.log(navbar);
+
+  if (window.scrollY < lastScroll) {
+    navbar.style.top = 0;
+  } else {
+    navbar.style.top = "-60px";
+  }
+  lastScroll = window.scrollY;
+});
+//? -------------------------------------------------------
+// let lastScroll = 0;
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY < lastScroll) {
+//     navbar.style.top = 0;
+//   } else {
+//     navbar.style.top = "-60px";
+//   }
+
+//   lastScroll = window.scrollY;
+// });
